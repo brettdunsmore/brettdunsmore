b4a90d2f0dc42f20bf00506c82c78b29a31584a0
brettdunsmore <dunsmore.brett@gmail.com>
2026-02-15 16:08:39 +1000

    Update resume.ts

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -61,9 +61,9 @@ export const profileData: ProfileData = {
       company: "Cloudflare",
       logo: "https://www.clipartmax.com/png/middle/146-1469376_the-free-version-of-cloudflare-content-delivery-network-cloudflare-logo-png.png",
       period: "October 2025 - Present",
-      description: "Public Sector, Queensland and Northern Territory",
+      description: "Public Sector",
       responsibilities: [
-        "Senior Named Account Executive, Australia"
+        "Senior Named Account Executive, Australia, Australia"
       ]
     },
     {
